ae47b5e0f7b9f5cd4f9fbb3eb0f1031c8b6c58eb
S Tunji Turner <vision@firstsightmedia.com>
2024-07-20 15:54:36 -0400

    Update App.swift

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -4,6 +4,6 @@ import Vercel
 struct App: RequestHandler {
 
     func onRequest(_ req: Request) async throws -> Response {
-        return .status(.ok).send("Hello, Swift")
+        return .status(.ok).send("Hello, My Swift")
     }
 }
